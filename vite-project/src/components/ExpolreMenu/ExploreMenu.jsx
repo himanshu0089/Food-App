c881e87a54e3202423891dc6e2fdd1cbe69f3aa8
himanshu0089 <88453380+himanshu0089@users.noreply.github.com>
2024-08-24 22:17:47 +0530

    feat: adding menu

--- vite-project/src/components/ExpolreMenu/ExploreMenu.jsx
+++ vite-project/src/components/ExpolreMenu/ExploreMenu.jsx
@@ -1,8 +1,23 @@
 import React from 'react'
-import './ExpolreMenu.css'
-function ExploreMenu() {
+import './ExploreMenu.css'
+import { menu_list } from '../../assets/assets'
+function ExploreMenu({category,setCategory}) {
   return (
-    <div>ExploreMenu</div>
+    <div className='explore-menu' id='explore-menu'>
+        <h1>Explore our menu</h1>
+        <p className="explore-menu-text">Choose from the diverse menu list and enhance your taste and valuable time with us</p>
+        <div className="explore-menu-list">
+       {menu_list.map((item,index)=>{
+         return(
+            <div onClick={()=>setCategory(prev=>prev===item.menu_name?"All":item.menu_name)} key={index} className="explore-menu-list-item">
+             <img className={category===item.menu_name?"active":""} src={item.menu_image} alt="" />
+             <p>{item.menu_name}</p>
+            </div>
+         )
+       })}
+       </div>
+       <hr />
+    </div>
   )
 }
 
